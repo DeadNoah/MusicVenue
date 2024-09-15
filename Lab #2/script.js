e73ddeda785dbc124a0c3624828733e936f0919a
DeadNoah <nsanche9@stumail.jccc.edu>
2024-09-15 16:17:16 -0500

    Add files via upload

---
+++ Lab #2/script.js
@@ -0,0 +1,24 @@
+        $(document).ready(function() {
+            var events = $('#event-list li');
+            events.each(function(index) {
+                if (index % 2 === 1) {
+                    $(this).addClass('grey');
+                }
+            })});
+
+
+        $(document).ready(function() {
+            $('#reviewForm').on('submit', function(event) {
+                event.preventDefault();
+                    var name = $('#name').val();
+                    var artist = $('#artist').val();
+            
+                    if (name && artist) { 
+                        $('#message').html(`<p>Thank you, ${name}! We will do our best to book ${artist}.</p>`).css('color', 'dark blue');
+
+                    } else {
+                        $('#message').html('<p>Please fill out all fields.</p>').css('color', 'red');
+                    }
+                });
+            });
+            
