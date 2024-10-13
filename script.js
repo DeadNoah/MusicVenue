beebb503f71308e4eb817ea4a9c6e7a4503b8bbc
DeadNoah <nsanche9@stumail.jccc.edu>
2024-10-13 14:28:48 -0500

    FINALLY

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        $(document).ready(function() {
+        $(document).ready(function () {
             var events = $('#event-list li');
             events.each(function(index) {
                 if (index % 2 === 1) {
@@ -24,16 +24,51 @@
     
 
     
-            $(document).ready(function() {
-                $('.hero').hover(
-                  function() {
-                    $(this).css('background-image', 'url(Images/show.jpeg)');
-                  },
-                  function() {
-                    $(this).css('background-image', 'url(Images/therino.jpeg)');
-                  }
-                );
-            });
+        $(document).ready(function () {
+        $('.hero').hover(
+          function() {
+            $(this).css('background-image', 'url(Images/show.jpeg)');
+          },
+          function() {
+            $(this).css('background-image', 'url(Images/therino.jpeg)');
+          }
+        );
+    });
 
+    $(document).ready(function () {
+        const slider = $('.slider');
+        const images = $('.slider img');
+        const totalImages = images.length;
+        let index = 0;
+      
+        function updateSlider() {
+          slider.css('transform', `translateX(${-index * 100}%)`);
+        }
+      
+        function nextImage() {
+          index = (index + 1) % totalImages;
+          updateSlider();
+        }
+      
+        function prevImage() {
+          index = (index - 1 + totalImages) % totalImages;
+          updateSlider();
+        }
+      
+        let autoPlay = setInterval(nextImage, 3000);
+      
 
-           
+        $('.next').click(function () {
+          clearInterval(autoPlay); 
+          nextImage();
+        });
+      
+        $('.prev').click(function () {
+          clearInterval(autoPlay);
+          prevImage();
+        });
+      
+        $('.next, .prev').click(function () {
+          autoPlay = setInterval(nextImage, 3000);
+        });
+      });
